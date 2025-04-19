e6e0ca400829d003b330b929e3a72052e85b78c4
Md Waliul Islam Alif <meetalif@gmail.com>
2025-04-19 18:17:18 +0600

    added circular progress indicator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -34,6 +34,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - radio
 - dropdown_form_field
 - dialog
+- circular_progress_indicator
 
 ### Changed
 
---
+++ lib/src/widgets/progress/circular_progress_indicator.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:waveui/waveui.dart';
+
+class WaveCircularProgressIndicator extends StatelessWidget {
+  const WaveCircularProgressIndicator({super.key, this.backgroundColor, this.value, this.color});
+  final Color? backgroundColor;
+  final Color? color;
+  final double? value;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = WaveApp.themeOf(context);
+    return CircularProgressIndicator(
+      backgroundColor: backgroundColor ?? theme.colorScheme.primary.withValues(alpha: .1),
+      color: color ?? theme.colorScheme.primary,
+      value: value,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(ColorProperty('backgroundColor', backgroundColor))
+      ..add(DoubleProperty('value', value))
+      ..add(ColorProperty('color', color));
+  }
+}
