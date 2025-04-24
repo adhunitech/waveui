b58c1393699f13c49b2eae416e923a1297156ffb
Md Waliul Islam Alif <meetalif@gmail.com>
2025-04-24 06:38:41 +0600

    added color option in divider

--- lib/src/widgets/divider.dart
+++ lib/src/widgets/divider.dart
@@ -4,21 +4,24 @@ import 'package:waveui/waveui.dart';
 
 class WaveDivider extends StatelessWidget {
   final Axis axis;
+  final Color? color;
 
   /// Creates a [WaveDivider] widget.
-  const WaveDivider({super.key, this.axis = Axis.horizontal});
+  const WaveDivider({super.key, this.axis = Axis.horizontal, this.color});
 
   @override
   Widget build(BuildContext context) {
     final theme = WaveApp.themeOf(context);
     return axis == Axis.horizontal
-        ? SizedBox(height: 1, width: double.infinity, child: ColoredBox(color: theme.colorScheme.divider))
-        : SizedBox(width: 1, height: double.infinity, child: ColoredBox(color: theme.colorScheme.divider));
+        ? SizedBox(height: 1, width: double.infinity, child: ColoredBox(color: color ?? theme.colorScheme.divider))
+        : SizedBox(width: 1, height: double.infinity, child: ColoredBox(color: color ?? theme.colorScheme.divider));
   }
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(EnumProperty<Axis>('axis', axis));
+    properties
+      ..add(EnumProperty<Axis>('axis', axis))
+      ..add(ColorProperty('color', color));
   }
 }
