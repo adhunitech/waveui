9fc085b8452e02ea8dc1fb2a26f177e86e2ca0f4
Md Waliul Islam Alif <meetalif@gmail.com>
2025-05-11 06:12:24 +0600

    added dialog sheet

--- lib/src/widgets/dialog/show_dialog.dart
+++ lib/src/widgets/dialog/show_dialog.dart
@@ -32,46 +32,61 @@ Future<T?> showWaveDialog<T>({
                   (context) => Stack(
                     children: [
                       GestureDetector(
-                        onTap:
-                            barrierDismissible
-                                ? () => Navigator.of(context).maybePop()
-                                : null,
+                        onTap: barrierDismissible ? () => Navigator.of(context).maybePop() : null,
                         // TODO: Fix background blur is not smooth
                         child: AnimatedBuilder(
                           animation: animation,
                           builder:
                               (context, child) => BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 3.0 * animation.value,
-                                  sigmaY: 3.0 * animation.value,
-                                ),
+                                filter: ImageFilter.blur(sigmaX: 3.0 * animation.value, sigmaY: 3.0 * animation.value),
                                 child: Container(
-                                  color: theme.colorScheme.barrier.withValues(
-                                    alpha: 0.2 * animation.value,
-                                  ),
+                                  color: theme.colorScheme.barrier.withValues(alpha: 0.2 * animation.value),
                                 ),
                               ),
                         ),
                       ),
-                      Center(
-                        child: SafeArea(
-                          top: useSafeArea,
-                          bottom: useSafeArea,
-                          child: builder(context),
-                        ),
-                      ),
+                      Center(child: SafeArea(top: useSafeArea, bottom: useSafeArea, child: builder(context))),
                     ],
                   ),
             ),
           ),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        final curvedAnimation = CurvedAnimation(
-          parent: animation,
-          curve: Curves.easeOut,
-          reverseCurve: Curves.easeIn,
-        );
+        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeOut, reverseCurve: Curves.easeIn);
         return FadeTransition(opacity: curvedAnimation, child: child);
       },
     ),
+  );
+}
+
+Future<void> showWaveDialogSheet({
+  required BuildContext context,
+  required String title,
+  required String message,
+  required List<Widget> actions,
+}) async {
+  await showWaveModalBottomSheet(
+    context: context,
+    builder:
+        (context) => SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(title, style: WaveApp.themeOf(context).textTheme.h4),
+                const SizedBox(height: 12),
+                Text(
+                  message,
+                  style: WaveApp.themeOf(
+                    context,
+                  ).textTheme.body.copyWith(color: WaveApp.themeOf(context).colorScheme.labelSecondary),
+                ),
+                const SizedBox(height: 16),
+                Row(spacing: 12, children: actions.map((e) => Expanded(child: e)).toList()),
+              ],
+            ),
+          ),
+        ),
   );
 }
