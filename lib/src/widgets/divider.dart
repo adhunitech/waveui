e3ea483264a16b95f9bd691c9aa0cec7abc8fa3c
Md Waliul Islam Alif <meetalif@gmail.com>
2025-05-22 04:11:01 +0600

    added WaveDividerType enum and dotted line support to WaveDivider for enhanced customization

--- lib/src/widgets/divider.dart
+++ lib/src/widgets/divider.dart
@@ -2,19 +2,45 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:waveui/waveui.dart';
 
+/// The type of divider to display.
+enum WaveDividerType {
+  /// A solid line divider
+  solid,
+
+  /// A dotted line divider
+  dotted,
+}
+
 class WaveDivider extends StatelessWidget {
   final Axis axis;
   final Color? color;
+  final WaveDividerType type;
 
   /// Creates a [WaveDivider] widget.
-  const WaveDivider({super.key, this.axis = Axis.horizontal, this.color});
+  const WaveDivider({super.key, this.axis = Axis.horizontal, this.color, this.type = WaveDividerType.solid});
 
   @override
   Widget build(BuildContext context) {
     final theme = WaveApp.themeOf(context);
+    final dividerColor = color ?? theme.colorScheme.divider;
+
+    if (type == WaveDividerType.dotted) {
+      return axis == Axis.horizontal
+          ? SizedBox(
+            height: 1,
+            width: double.infinity,
+            child: CustomPaint(painter: DottedLinePainter(color: dividerColor, isHorizontal: true)),
+          )
+          : SizedBox(
+            width: 1,
+            height: double.infinity,
+            child: CustomPaint(painter: DottedLinePainter(color: dividerColor, isHorizontal: false)),
+          );
+    }
+
     return axis == Axis.horizontal
-        ? SizedBox(height: 1, width: double.infinity, child: ColoredBox(color: color ?? theme.colorScheme.divider))
-        : SizedBox(width: 1, height: double.infinity, child: ColoredBox(color: color ?? theme.colorScheme.divider));
+        ? SizedBox(height: 1, width: double.infinity, child: ColoredBox(color: dividerColor))
+        : SizedBox(width: 1, height: double.infinity, child: ColoredBox(color: dividerColor));
   }
 
   @override
@@ -22,6 +48,43 @@ class WaveDivider extends StatelessWidget {
     super.debugFillProperties(properties);
     properties
       ..add(EnumProperty<Axis>('axis', axis))
-      ..add(ColorProperty('color', color));
+      ..add(ColorProperty('color', color))
+      ..add(EnumProperty<WaveDividerType>('type', type));
   }
+}
+
+class DottedLinePainter extends CustomPainter {
+  final Color color;
+  final bool isHorizontal;
+
+  DottedLinePainter({required this.color, required this.isHorizontal});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..color = color
+          ..strokeWidth = 1;
+
+    const dashWidth = 8.0;
+    const dashSpace = 6.0;
+    double startX = 0;
+    double startY = 0;
+
+    if (isHorizontal) {
+      while (startX < size.width) {
+        canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+        startX += dashWidth + dashSpace;
+      }
+    } else {
+      while (startY < size.height) {
+        canvas.drawLine(Offset(0, startY), Offset(0, startY + dashWidth), paint);
+        startY += dashWidth + dashSpace;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DottedLinePainter oldDelegate) =>
+      color != oldDelegate.color || isHorizontal != oldDelegate.isHorizontal;
 }
